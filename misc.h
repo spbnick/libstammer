c160a532a430850e77c8a191b08d6501b7e2f7e6
Nikolai Kondrashov <spbnick@gmail.com>
2016-10-03 23:03:29 +0300

    Add misc.h

---
+++ misc.h
@@ -0,0 +1,11 @@
+/*
+ * Miscellaneous definitions
+ */
+
+#ifndef _MISC_H
+#define _MISC_H
+
+/** Return number of elements in an array */
+#define ARRAY_SIZE(_x) (sizeof(_x) / sizeof((_x)[0]))
+
+#endif /* _MISC_H */
