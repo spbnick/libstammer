944216a9efe7889f894c61626929370ad8b45a20
Nikolai Kondrashov <spbnick@gmail.com>
2017-12-29 10:18:21 +0200

    Add MIN/MAX macros

--- misc.h
+++ misc.h
@@ -8,4 +8,10 @@
 /** Return number of elements in an array */
 #define ARRAY_SIZE(_x) (sizeof(_x) / sizeof((_x)[0]))
 
+/** Return minimum */
+#define MIN(_x, _y) ((_x) < (_y) ? (_x) : (_y))
+
+/** Return maximum */
+#define MAX(_x, _y) ((_x) > (_y) ? (_x) : (_y))
+
 #endif /* _MISC_H */
