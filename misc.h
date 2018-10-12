5990dd67e2dbe2700bfa744a34f08dd6311ea3ba
Nikolai Kondrashov <spbnick@gmail.com>
2018-10-12 11:38:14 +0300

    Add a basic assert macro

--- misc.h
+++ misc.h
@@ -14,4 +14,23 @@
 /** Return maximum */
 #define MAX(_x, _y) ((_x) > (_y) ? (_x) : (_y))
 
+/**
+ * Assert an expression is true.
+ * Go into an endless loop if the expression is false.
+ *
+ * @param _expr The expression to assert.
+ */
+#ifndef NDEBUG
+#define assert(_expr) \
+    do {                        \
+        if (!(_expr)) {         \
+            do {                \
+                asm ("wfi");    \
+            } while (1);        \
+        }                       \
+    } while (0)
+#else
+#define assert(_expr)
+#endif
+
 #endif /* _MISC_H */
