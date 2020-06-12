379843c9e822d3924b4049763fe151f473ff1209
Nikolai Kondrashov <spbnick@gmail.com>
2020-06-12 11:03:36 +0300

    misc: Fix memset/memcpy formatting

--- misc.c
+++ misc.c
@@ -4,7 +4,8 @@
 
 #include <misc.h>
 
-void *memset(void *s, int c, size_t n) {
+void *
+memset(void *s, int c, size_t n) {
     char *p = s;
     for (; n > 0; p++, n--) {
         *p = c;
@@ -12,7 +13,8 @@ void *memset(void *s, int c, size_t n) {
     return s;
 }
 
-void *memcpy(void *dest, const void *src, size_t n) {
+void *
+memcpy(void *dest, const void *src, size_t n) {
     char *d = dest;
     const char *s = src;
     for (; n > 0; d++, s++, n--) {
